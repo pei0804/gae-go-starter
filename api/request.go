94f2c6199692f68da501f12abfe9159bd9bcecd0
tikasan <satak47cpc@gmail.com>
2017-02-16 13:30:14 +0900

    fix: returnするために返り値の設定をした

--- api/request.go
+++ api/request.go
@@ -10,19 +10,19 @@ import (
 type Request struct {
 }
 
-func (r *Request) GetTest(c echo.Context) {
+func (r *Request) GetTest(c echo.Context) error {
 	name := c.QueryParam("name")
 	return c.JSON(http.StatusOK, fmt.Sprintf("GET %s", name))
 }
 
-func (r *Request) PostTest(c echo.Context) {
+func (r *Request) PostTest(c echo.Context) error {
 	return c.JSON(http.StatusOK, "POST")
 }
 
-func (r *Request) PutTest(c echo.Context) {
+func (r *Request) PutTest(c echo.Context) error {
 	return c.JSON(http.StatusOK, "PUT")
 }
 
-func (r *Request) DeleteTest(c echo.Context) {
+func (r *Request) DeleteTest(c echo.Context) error {
 	return c.JSON(http.StatusOK, "DELETE")
 }
