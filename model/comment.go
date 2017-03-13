ccea3f56f1f66749932d9ef027c96ef70736e8f4
tikasan <satak47cpc@gmail.com>
2017-03-13 12:44:12 +0900

    add: basemodel

--- model/comment.go
+++ model/comment.go
@@ -1,12 +1,7 @@
 package model
 
-import "time"
-
 type Comments struct {
-	ID        int        `gorm:"AUTO_INCREMENT" json:"id"`
-	Name      string     `gorm:"type:varchar(30)" json:"name"`
-	Comment   string     `gorm:"type:text" json:"comment"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
+	BaseModel
+	Name    string `gorm:"type:varchar(30)" json:"name"`
+	Comment string `gorm:"type:text" json:"comment"`
 }
